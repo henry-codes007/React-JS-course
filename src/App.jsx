edf1d59e0135be44fa97642da1340631506cb27c
Nsikak Akpan <builtbykaksy@gmail.com>
2024-10-28 09:59:22 +0100

    starter code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <section className='home'>
+     <div>
+       <h1>Welcome to our React.js Course</h1>
+       <p>I promise that you won&apos;t regret it</p>
+     </div>
+    </section>
+  )
+}
+
+export default App
