5eb3d501410949a0350c24ac3bfa34a2917fc785
Nsikak Akpan <builtbykaksy@gmail.com>
2024-11-06 21:45:52 +0100

    update to movie page

---
+++ src/components/moviecard/MovieCard.jsx
@@ -0,0 +1,18 @@
+
+
+const MovieCard = () => {
+  return (
+    <div>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            <h1>title:</h1>
+            <h3>description:</h3>
+            <p>rating:</p>
+        </div>
+    </div>
+  )
+}
+
+export default MovieCard
--- src/pages/Moviespage.jsx
+++ src/pages/Moviespage.jsx
@@ -1,21 +1,67 @@
+import { useEffect, useState } from "react";
 
 
 const Moviespage = () => {
-    // const api_key = "a3ec5cef31325905723a0ad860da9c7e";
+    const api_key = "a3ec5cef31325905723a0ad860da9c7e";
+    const baseUrl = "https://image.tmdb.org/t/p/original/";
+    const [trendingMovies, setTrendingMovies] = useState([]);
+    const [isLoading, setIsloading] = useState(false);
 
-    // const requests = {
-    //     fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${api_key}&language=en-US`,
-    //     fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&with_network=213`,
-    //     fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=en-US`,
-    //     fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=28`,
-    //     fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=35`,
-    //     fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=27`,
-    //     fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=10749`,
-    //     fetchDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=99`
-    // };
+    const requests = {
+        fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${api_key}&language=en-US`,
+        fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&with_network=213`,
+        fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=en-US`,
+        fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=28`,
+        fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=35`,
+        fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=27`,
+        fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=10749`,
+        fetchDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=99`
+    };
     
+    const getTrendingMovies = async() => {
+      setIsloading(true);
+      try {
+        const response = await fetch(requests.fetchTrending);
+        const data = await response.json();
+        // console.log(data.results);
+        setTrendingMovies(data.results);
+        setIsloading(false);
+      } catch (error) {
+        console.log(error);
+        setIsloading(false);
+      }      
+    }
+
+    useEffect(() => {
+      console.log({trendingMovies})
+    }, [trendingMovies]);
+
+    useEffect(() => {
+      getTrendingMovies();
+    }, [])
+
   return (
-    <div>Moviespage</div>
+    <div>
+      {
+        isLoading ? (
+          <h1>Loading.....</h1>
+        ) : (
+          trendingMovies.length > 0 ?
+        trendingMovies.map(movie => (
+          <div key={movie.id}>
+            <img src={`${baseUrl}${movie.backdrop_path}`} alt={movie.title} />
+            <h3>{movie.title}</h3>
+            <p>{movie.overview}</p>
+          </div>
+        )) : (
+          <h1>
+            Oops!!, you have no movies
+          </h1>
+        )
+        )
+      }
+      
+    </div>
   )
 }
 
